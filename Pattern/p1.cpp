0f3f11a14f9f14739889d6f273b508a07e226432
Sumit Singh <130147865+sumit0408@users.noreply.github.com>
2023-12-19 16:45:24 +0530

    Add files via upload

---
+++ Pattern/p1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    system("CLS");
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
