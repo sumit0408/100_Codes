b7a275077744633e29f28c1810c2d173f5e2bc8f
Sumit Singh <130147865+sumit0408@users.noreply.github.com>
2023-12-19 15:05:00 +0530

    Add files via upload

---
+++ Array/largest_number_in_array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int largest_number(int arr[], int n)
+{
+    int maxi=INT_MIN;
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>maxi)
+        {
+            maxi=arr[i];
+        }
+    }
+    return maxi;
+}
+int main()
+{
+    system("CLS");
+    int n;
+    cout<<"Enter the size of an Array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of an Array : ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    int largest=largest_number(arr,n);
+    cout<<"Largest Number in an Array : "<<largest;
+    return 0;
+
+}
