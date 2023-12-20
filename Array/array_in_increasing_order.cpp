c605e0c16e4d99be3f53258f8ff4f070e5ab2089
Sumit Singh <130147865+sumit0408@users.noreply.github.com>
2023-12-20 12:03:45 +0530

    Add files via upload

---
+++ Array/array_in_increasing_order.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+void increasing_order(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i; j<n; j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                int tmp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=tmp;
+            }
+        }
+    }
+    print(arr,n);
+}
+
+int main()
+{
+    system("CLS");
+    int n;
+    cout<<"Enter the size of an Array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of an Array : ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    increasing_order(arr,n);
+    return 0;
+
+}
