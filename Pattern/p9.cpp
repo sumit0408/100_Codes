f324018cca16db987f3564f5cba9b1766d8bc4ab
Sumit Singh <130147865+sumit0408@users.noreply.github.com>
2023-12-20 11:25:17 +0530

    Add files via upload

---
+++ Pattern/p9.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    system("CLS");
+    int n;
+    cout<<"Enter the value of N : ";
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0;  j<(n-i-1); j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;  k<(2*i+1); k++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+        for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0; k<(2*(n-i)-1); k++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
