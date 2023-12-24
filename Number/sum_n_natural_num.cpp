16f641d754ca4e2603f451b7c95fa5247e357047
Sumit Singh <130147865+sumit0408@users.noreply.github.com>
2023-12-24 09:41:59 +0530

    Add files via upload

---
+++ Number/sum_n_natural_num.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    system("CLS");
+    int n;
+    cout<<"Enter the number of Terms : ";
+    cin>>n;
+    int ans = (n*(n+1))/2;
+    cout<<"The sum of first n terms is : "<<ans<<endl;
+    return 0;
+}
