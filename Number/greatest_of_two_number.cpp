2b5192526fc2961507839bd83894effa4193d657
Sumit Singh <130147865+sumit0408@users.noreply.github.com>
2023-12-24 14:52:35 +0530

    Add files via upload

---
+++ Number/greatest_of_two_number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    system("CLS");
+    int a,b;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    cout<<"Enter the value of b : ";
+    cin>>b;
+    if(a>b)
+        cout<<"a is greater.";
+    else
+        cout<<"b is greater.";
+    return 0;
+}
