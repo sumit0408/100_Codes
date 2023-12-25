06d653c42552fee55504280699c356274dcc76f8
Sumit Singh <130147865+sumit0408@users.noreply.github.com>
2023-12-25 08:36:34 +0530

    Add files via upload

---
+++ Number/area_of_circle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    system("CLS");
+    float r, area;
+    cout<<"Enter the Radius : ";
+    cin>>r;
+    area = 3.14*r*r;
+    cout<<"Area of a circle is "<<area<<" unit sq.";
+    return 0;
+}
